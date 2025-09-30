18d9b3a7f0e32cb1601220abef5394847de2e022
kireetivar <kireeti3901@gmail.com>
2025-09-30 23:20:43 +0530

    construct inverted index

--- main.go
+++ main.go
@@ -1,7 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Start")
+
+	AllFiles := TraverseDir("test",1)
+
+	index := createIndex(AllFiles)
+
+	fmt.Println(index)
+}
+
+func TraverseDir(s string, depth int) []string{
+	dir, err := os.ReadDir(s)
+	// TODO handle error properly
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		os.Exit(1)
+	}
+	var ds []string
+
+	for _, v := range dir {
+		fullpath := filepath.Join(s, v.Name())
+		if v.IsDir() {
+			ds = append(ds, TraverseDir(fullpath, 1)...)
+			depth--;
+			if depth<=0{
+				return ds
+			}
+		}else{
+			ds = append(ds, fullpath)
+		}
+	}
+	return ds
+}
+
+
+func createIndex(allfiles []string) map[string][]string{
+	srcMap := make(map[string]string)
+	for _,v := range allfiles{
+		bs,err := os.ReadFile(v)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			os.Exit(1)
+		}
+		contents := string(bs)
+		srcMap[v] = contents
+	}
+
+	index := make(map[string][]string)
+
+
+	for k,v := range srcMap {
+		keywords := strings.Fields(v)
+
+
+		for _, v := range keywords {
+			value ,ok := index[v]
+			if ok {
+				index[v] = append(value, k)
+			}else {
+				index[v] = []string{k}
+			}
+		}
+	}
+
+	return index
 }
